4eb03479ac86d798297281a6927288b4e639c53f
Pol <pol.segu@enti.cat>
2020-10-07 23:45:14 +0200

    1

--- AA1_01_XML/AA1_01_XML/main.cpp
+++ AA1_01_XML/AA1_01_XML/main.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream> 
 #include <vector> 
+#include <list> 
 #include "rapidxml.hpp"
 #include "rapidxml_utils.hpp"
 #include "rapidxml_iterators.hpp"
@@ -13,13 +14,21 @@ int main()
 
 	std::ifstream file ("config.xml");
 	std::string str;
-	print(std::back_inserter(str), doc, 0);
+	rapidxml::print(std::back_inserter(str), doc, 0);
 	file.close();
 
 	std::cout << str;
 
 
+	/*
+	rapidxml::xml_node<> *pRoot = doc.first_node();
+	std::list<enemy> enemyList
+	
+	enemyList.vit = doc
 
+	*/
+
+	return 0;
 }
 
 
