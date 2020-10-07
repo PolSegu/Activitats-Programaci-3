88c03071e79beb4a5c3facc8632e09ce68d60937
Pol <pol.segu@enti.cat>
2020-10-07 22:27:11 +0200

    1

--- AA1_01_XML/AA1_01_XML/main.cpp
+++ AA1_01_XML/AA1_01_XML/main.cpp
@@ -1,1 +1,53 @@
 //First exercice
+
+#include <iostream> 
+#include <vector> 
+#include "rapidxml.hpp"
+#include "rapidxml_utils.hpp"
+#include "rapidxml_iterators.hpp"
+#include "rapidxml_print.hpp"
+
+int main()
+{
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
