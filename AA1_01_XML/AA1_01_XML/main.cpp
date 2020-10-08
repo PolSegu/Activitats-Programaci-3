b776f24ef0c2e36f6db81816053642a35f4c0511
Pol <pol.segu@enti.cat>
2020-10-08 18:05:09 +0200

    1

--- AA1_01_XML/AA1_01_XML/main.cpp
+++ AA1_01_XML/AA1_01_XML/main.cpp
@@ -2,30 +2,46 @@
 
 #include <iostream> 
 #include <vector> 
+#include <fstream> 
+#include <sstream> 
 #include <list> 
 #include "rapidxml.hpp"
 #include "rapidxml_utils.hpp"
 #include "rapidxml_iterators.hpp"
 #include "rapidxml_print.hpp"
+#include "enemy.h"
+#include "weapon.h"
 
 int main()
 {
 	rapidxml::xml_document<> doc;
-
-	std::ifstream file ("config.xml");
-	std::string str;
-	rapidxml::print(std::back_inserter(str), doc, 0);
+	std::ofstream file("config.xml");
+	std::stringstream buffer;
+	buffer << file.rdbuf();
 	file.close();
+	std::string content(buffer.str());
+	doc.parse<0>(&content[0]);
 
-	std::cout << str;
+	rapidxml::xml_node<>* pRoot = doc.first_node();
 
+	std::list<enemy> enemyList;
+
+	for (rapidxml::xml_node<>* pNode = pRoot->first_node("Enemy"); pNode; pNode = pNode->next_sibling())
+	{
+		std::cout << pNode->name() << ':' << '\n';
+
+		for (rapidxml::xml_node<>* pNodeI = pNode->first_node(); pNodeI; pNodeI = pNodeI->next_sibling())
+		{
+			std::cout << pNodeI->name() << ':' << pNodeI->value() << '\n';
+		};
+		std::cout << '\n';
+	}
 
 	/*
 	rapidxml::xml_node<> *pRoot = doc.first_node();
 	std::list<enemy> enemyList
 	
-	enemyList.vit = doc
-
+	enemyList.vit = doc;
 	*/
 
 	return 0;
